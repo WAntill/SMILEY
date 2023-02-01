d29574c6eb52ea3ae914185345734a68ba3b7dee
William Antill <william.antill1999@outlook.com>
2023-02-01 14:35:12 +0000

    added new Label at the top the display an image for what the device is

--- UI.py
+++ UI.py
@@ -6,9 +6,10 @@ import tkinter as tk
 root = tk.Tk()
 root.geometry("500x500")
 root.title("Feedback Machine")
-root.config(bg="skyblue")
+root.config(bg="black")
 
 Grid.rowconfigure(root, 0, weight=1)
+Grid.rowconfigure(root, 1, weight=1)
 
 Grid.columnconfigure(root, 0, weight=1)
 Grid.columnconfigure(root, 1, weight=1)
@@ -16,25 +17,30 @@ Grid.columnconfigure(root, 2, weight=1)
 Grid.columnconfigure(root, 3, weight=1)
 Grid.columnconfigure(root, 4, weight=1)
 
+title_frame = tk.Frame(root, bg="white")
+title_frame.grid(row=0, column=1, columnspan=3, padx=10, pady=10, sticky="nsew")
+
 left_frame = tk.Frame(root, bg="grey")
-left_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+left_frame.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
 
 left_middle_frame = tk.Frame(root, bg="grey")
-left_middle_frame.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
+left_middle_frame.grid(row=1, column=1, padx=10, pady=10, sticky="nsew")
 
 middle_frame = tk.Frame(root, bg="grey")
-middle_frame.grid(row=0, column=2, padx=10, pady=10, sticky="nsew")
+middle_frame.grid(row=1, column=2, padx=10, pady=10, sticky="nsew")
 
 right_middle_frame = tk.Frame(root, bg="grey")
-right_middle_frame.grid(row=0, column=3, padx=10, pady=10, sticky="nsew")
+right_middle_frame.grid(row=1, column=3, padx=10, pady=10, sticky="nsew")
 
 right_frame = tk.Frame(root, bg="grey")
-right_frame.grid(row=0, column=4, padx=10, pady=10, sticky="nsew")
+right_frame.grid(row=1, column=4, padx=10, pady=10, sticky="nsew")
+
+label1 = Label(title_frame, anchor=CENTER, text="Pav is a Neek", font=('Calibri', 26), bg="white").pack()
 
 image_excellent = PhotoImage(file="Ecstatic Face.png")
 image_good = PhotoImage(file="Happy Face.png")
 image_okay = PhotoImage(file="OK Face.png")
-image_bad = PhotoImage(file="Unhappy Face.png")
+image_bad = PhotoImage(file="Not Happy Face.png")
 image_very_bad = PhotoImage(file="Angry Face.png")
 
 def RegisterResponseExcellent():
