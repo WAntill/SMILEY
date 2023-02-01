b8bfc2505d720413ee070cf53138edc515583423
William Antill <william.antill1999@outlook.com>
2023-02-01 14:09:51 +0000

    completed basic UI for feedback response

--- UI.py
+++ UI.py
@@ -4,33 +4,38 @@ from tkinter import *
 import tkinter as tk
 
 root = tk.Tk()
+root.geometry("500x500")
 root.title("Feedback Machine")
 root.config(bg="skyblue")
 
+Grid.rowconfigure(root, 0, weight=1)
+
+Grid.columnconfigure(root, 0, weight=1)
+Grid.columnconfigure(root, 1, weight=1)
+Grid.columnconfigure(root, 2, weight=1)
+Grid.columnconfigure(root, 3, weight=1)
+Grid.columnconfigure(root, 4, weight=1)
+
 left_frame = tk.Frame(root, bg="grey")
-left_frame.grid(row=0, column=1, padx=10, pady=10, sticky=tk.W)
+left_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
 
 left_middle_frame = tk.Frame(root, bg="grey")
-left_middle_frame.grid(row=0, column=2, padx=10, pady=10, sticky=tk.W)
+left_middle_frame.grid(row=0, column=1, padx=10, pady=10, sticky="nsew")
 
 middle_frame = tk.Frame(root, bg="grey")
-middle_frame.grid(row=0, column=3, padx=10, pady=10, sticky=tk.W)
+middle_frame.grid(row=0, column=2, padx=10, pady=10, sticky="nsew")
 
 right_middle_frame = tk.Frame(root, bg="grey")
-right_middle_frame.grid(row=0, column=4, padx=10, pady=10, sticky=tk.W)
+right_middle_frame.grid(row=0, column=3, padx=10, pady=10, sticky="nsew")
 
 right_frame = tk.Frame(root, bg="grey")
-right_frame.grid(row=0, column=5, padx=10, pady=10, sticky=tk.W)
+right_frame.grid(row=0, column=4, padx=10, pady=10, sticky="nsew")
 
-root.columnconfigure(0, weight=1)
-root.columnconfigure(6, weight=1)
-root.rowconfigure(0, weight=1)
-
-image_excellent = PhotoImage(file="download.png")
-# image_good = PhotoImage(file="")
-image_okay = PhotoImage(file="straight.png")
-# image_bad = PhotoImage(file="")
-image_very_bad = PhotoImage(file="Sad.png")
+image_excellent = PhotoImage(file="Ecstatic Face.png")
+image_good = PhotoImage(file="Happy Face.png")
+image_okay = PhotoImage(file="OK Face.png")
+image_bad = PhotoImage(file="Unhappy Face.png")
+image_very_bad = PhotoImage(file="Angry Face.png")
 
 def RegisterResponseExcellent():
     print("Glad to hear it was excellent")
@@ -47,19 +52,19 @@ def RegisterResponseBad():
 def RegisterResponseVeryBad():
     print("Sorry to hear it was bad")
 
-button_excellent = tk.Button(left_frame, text="Excellent", image=image_excellent, command=RegisterResponseExcellent)
+button_excellent = tk.Button(left_frame, text="Excellent", bg="green", image=image_excellent, command=RegisterResponseExcellent)
 button_excellent.pack(fill=tk.BOTH, expand=True)
 
-# button_good = tk.button(left_middle_frame, text="Good", image= , command=RegisterResponseGood)
-# button_good.pack(fill=tk.BOTH, expand=True)
+button_good = tk.Button(left_middle_frame, text="Good", bg="lightgreen", image=image_good , command=RegisterResponseGood)
+button_good.pack(fill=tk.BOTH, expand=True)
 
-button_straight = tk.Button(middle_frame, text="Okay", image=image_okay, command=RegisterResponseOkay)
+button_straight = tk.Button(middle_frame, text="Okay", bg="yellow", image=image_okay, command=RegisterResponseOkay)
 button_straight.pack(fill=tk.BOTH, expand=True)
 
-# button_bad = tk.button(right_middle_frame, text="Bad", image= , command=RegisterResponseBad)
-# button_bad.pack(fill=tk.BOTH, expand=True)
+button_bad = tk.Button(right_middle_frame, text="Bad", bg="orange", image=image_bad , command=RegisterResponseBad)
+button_bad.pack(fill=tk.BOTH, expand=True)
 
-button_very_bad = tk.Button(right_frame, text="Very Bad", image=image_very_bad , command=RegisterResponseBad)
+button_very_bad = tk.Button(right_frame, text="Very Bad", bg="red", image=image_very_bad , command=RegisterResponseBad)
 button_very_bad.pack(fill=tk.BOTH, expand=True)
 
 root.mainloop()
